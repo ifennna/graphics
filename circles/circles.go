770d37995732981e3eb5772be63404ff331cda12
Ifenna Awagu <jawagu@gmail.com>
2019-04-29 21:56:12 +0100

    Fix pixel mirroring in circle drawing

--- circles/circles.go
+++ circles/circles.go
@@ -13,13 +13,13 @@ func Draw(midpoint pixel.Point, radius float64, algorithm string) {
 
 func drawMirroredPixels(midpoint pixel.Point, point pixel.Point) {
 	sectorOne := pixel.Point{X: point.X + midpoint.X, Y: point.Y + midpoint.Y}
-	sectorTwo := pixel.Point{X: point.Y + midpoint.Y, Y: point.X + midpoint.X}
+	sectorTwo := pixel.Point{X: point.Y + midpoint.X, Y: point.X + midpoint.Y}
 	sectorThree := pixel.Point{X: -point.X + midpoint.X, Y: point.Y + midpoint.Y}
-	sectorFour := pixel.Point{X: -point.Y + midpoint.Y, Y: point.X + midpoint.X}
+	sectorFour := pixel.Point{X: -point.Y + midpoint.X, Y: point.X + midpoint.Y}
 	sectorFive := pixel.Point{X: -point.X + midpoint.X, Y: -point.Y + midpoint.Y}
-	sectorSix := pixel.Point{X: -point.Y + midpoint.Y, Y: -point.X + midpoint.X}
+	sectorSix := pixel.Point{X: -point.Y + midpoint.X, Y: -point.X + midpoint.Y}
 	sectorSeven := pixel.Point{X: point.X + midpoint.X, Y: -point.Y + midpoint.Y}
-	sectorEight := pixel.Point{X: point.Y + midpoint.Y, Y: -point.X + midpoint.X}
+	sectorEight := pixel.Point{X: point.Y + midpoint.X, Y: -point.X + midpoint.Y}
 
 	pixel.DrawPixel(sectorOne)
 	pixel.DrawPixel(sectorTwo)
