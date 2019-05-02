727fb1dfd271226fe7828d4573cfcebdac7c2601
Ifenna Awagu <jawagu@gmail.com>
2019-05-02 22:59:25 +0100

    Set up translation of shapes

--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"github.com/go-gl/gl/all-core/gl"
 	"github.com/go-gl/glfw/v3.2/glfw"
-	"graphics/arcs"
-	"graphics/ellipses"
-	"graphics/lines"
+	"graphics/circles"
 	"graphics/pixel"
 	"runtime"
 )
+
+var position = 0
+const distance = 60
 
 func init() {
 	runtime.LockOSThread()
@@ -29,22 +30,30 @@ func draw(window *glfw.Window, program uint32) {
 	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 	gl.UseProgram(program)
 
-	run()
+	midpoint := pixel.Point{float64(position), 0}
+
+	if position > distance {
+		position = 0
+	}
+
+	run(midpoint)
+
+	position++
 
 	window.SwapBuffers() // swap rendering and drawing buffers
 	glfw.PollEvents()    // poll input events
 }
 
-func run() {
-	//circles.Draw(pixel.Point{0, 0}, 19, "midpoint")
+func run(midpoint pixel.Point) {
+	circles.Draw(midpoint, 19, "midpoint")
 	//circles.Draw(pixel.Point{10, 0}, 29, "bresenhams")
 
-	lines.Draw(pixel.Point{X: 0, Y: 20}, pixel.Point{X: 50, Y: 20}, "bresenhams")
-	lines.Draw(pixel.Point{X: 0, Y: 0}, pixel.Point{X: 50, Y: 0}, "dda")
-
-	ellipses.Draw(pixel.Point{X: 0, Y: 10}, 3, 10)
-	ellipses.Draw(pixel.Point{X: 50, Y: 10}, 3, 10)
-
-	arcs.Draw(pixel.Point{X: 25, Y: 45}, 35, true)
-	arcs.Draw(pixel.Point{X: 25, Y: -25}, 35, false)
+	//lines.Draw(pixel.Point{X: 0, Y: 20}, pixel.Point{X: 50, Y: 20}, "bresenhams")
+	//lines.Draw(pixel.Point{X: 0, Y: 0}, pixel.Point{X: 50, Y: 0}, "dda")
+	//
+	//ellipses.Draw(pixel.Point{X: 0, Y: 10}, 3, 10)
+	//ellipses.Draw(pixel.Point{X: 50, Y: 10}, 3, 10)
+	//
+	//arcs.Draw(pixel.Point{X: 25, Y: 45}, 35, true)
+	//arcs.Draw(pixel.Point{X: 25, Y: -25}, 35, false)
 }
